fa1ea61ea20439c627b9fc87c08556eebe89000a
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-09-16 10:15:06 +0200

    round

--- vnet/VizOneat.py
+++ vnet/VizOneat.py
@@ -203,7 +203,7 @@ class VizOneat(object):
             max_activation = np.sum(activation, axis = -1)
             max_activation = normalizeFloatZeroOne(max_activation, 1, 99.8, dtype = self.dtype)
             if len(max_activation.shape) == 4:
-               padz = round((self.pad_width[0] - max_activation.shape[-3])/2)
+               padz = round((self.pad_width[0] - max_activation.shape[-3]))/2
                pady = round((self.pad_width[1] - max_activation.shape[-2]))/2
                padx = round((self.pad_width[2]- max_activation.shape[-1]))/2
                
