c39552fd5f673cd10dad09179c486309d0064d6c
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-09-16 09:59:51 +0200

    monster

--- vnet/VizOneat.py
+++ vnet/VizOneat.py
@@ -204,7 +204,7 @@ class VizOneat(object):
             max_activation = normalizeFloatZeroOne(max_activation, 1, 99.8, dtype = self.dtype)
             if len(max_activation.shape) == 4:
                
-               max_activation_new = np.pad(max_activation, ((0,0),((self.pad_width[0] - max_activation.shape[-3])//2,(self.pad_width[0] - max_activation.shape[-3])//2),((self.pad_width[1] - max_activation.shape[-2])//2,(self.pad_width[1] - max_activation.shape[-2])//2), ((self.pad_width[2]- max_activation.shape[-1]))//2,(self.pad_width[2]- max_activation.shape[-1]))//2))
+               max_activation_new = np.pad(max_activation, ((0,0),((self.pad_width[0] - max_activation.shape[-3])//2,(self.pad_width[0] - max_activation.shape[-3])//2),((self.pad_width[1] - max_activation.shape[-2])//2,(self.pad_width[1] - max_activation.shape[-2])//2), ((self.pad_width[2]- max_activation.shape[-1])//2,(self.pad_width[2]- max_activation.shape[-1]))//2))
                
             if len(max_activation.shape) == 3:
                 max_activation_new = np.pad(max_activation, ((0,0),((self.pad_width[0]- max_activation.shape[-2])//2,(self.pad_width[0]- max_activation.shape[-2])//2), ((self.pad_width[1]- max_activation.shape[-1])//2,(self.pad_width[1]- max_activation.shape[-1])//2)))
